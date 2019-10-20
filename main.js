6bab811a8e3e091d1c6b033ae4e94ab0260db5db
Hans Vos <hans.vos@codeimmersives.com>
2019-10-20 14:42:43 -0400

    first commit

--- main.js
+++ main.js
@@ -1,26 +1,37 @@
-function changeLast() {
-
+function changeLast(arr, num) {
+  arr.pop();
+  arr.push(num);
 }
 
-function addMeToEnd() {
-
+function addMeToEnd(arr) {
+  arr.push('Colin');
 }
 
-function addMeToStart() {
-
+function addMeToStart(arr) {
+  arr.unshift('Colin');
 }
 
-function changeAllValuesTo() {
-
+function changeAllValuesTo(arr, value) {
+  for(i = 0; i < arr.length; ++i){
+    arr[i] = value;
+  } 
 }
 
-function oddOrEven() {
-
+function oddOrEven(arr) {
+  for(i = 0; i < arr.length; ++i){
+   if(arr[i] % 2 === 0) {
+     newArray = "even";
+   } else if (arr[i] % 2 === 0) {
+     newArray = "odd";
+   }
+ }
 }
 
-function changeNextThreeToValue() {
-
+function changeNextThreeToValue(start, arr, value) {
+  
 }
+// const value1 = 15;
+// expect(arr1).toEqual([1, 2, 15, 15, 15, 9]);
 
 module.exports = {
   changeLast,
@@ -30,3 +41,7 @@ module.exports = {
   oddOrEven,
   changeNextThreeToValue,
 }
+
+
+
+// ✕ starting with the given index, it changes three consecutive values in the given array to the given value (1ms)
